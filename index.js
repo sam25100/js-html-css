5a305f691d96a8b4500eae725eef268de60bad24
Programmer <samuelpistosvernaz@gmail.com>
2025-04-06 18:44:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-console.log(`Hello`);
-console.log(`I like pizza`);
+// Display messages on the webpage
+const messageContainer = document.getElementById("messageContainer");
 
-window.alert(`Alert, your computer has been hacked
-i have stolen all of your data files, be prepared to give me 20000 usd cash or lose all data!`)
+// Print messages to the page instead of the console
+messageContainer.innerHTML = `<p>Hello</p><p>I like pizza</p>`;
+
+// Trigger a window alert
+window.alert(`Alert: Your computer has been hacked! 
+I have stolen all of your data files. Be prepared to give me 20000 USD cash or lose all data!`);
