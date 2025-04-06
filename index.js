eab7c666961a2ecaaecc26f3b6dfec1db5e8c61a
Programmer <samuelpistosvernaz@gmail.com>
2025-04-06 18:25:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log(`Hello`);
+console.log(`I like pizza`);
+
+window.alert(`Alert, your computer has been hacked
+i have stolen all of your data files, be prepared to give me 20000 usd cash or lose all data!`)
